62c106ab462b546ab4abd62160c1b597b4aa46a5
Armen733 <armo2009@mail.ru>
2022-03-30 14:12:05 +0400

    homework

--- lessongit.py
+++ lessongit.py
@@ -1,2 +1,48 @@
-x = 5
-print(x)
+# word = 'exercises'
+# list_tarer = []
+# list_tver = []
+# my_dict = {}
+# for i in word:
+#     list_tver.append(word.count(i))
+#     list_tarer.append(i)
+# for x,y in zip(list_tarer,list_tver):
+#     my_dict.setdefault(x,y)
+# print(my_dict)
+
+# a = 'a, 2, b, 5, c, 8, a, 4, e, 11'
+# a = a.split(', ')
+# mydict = {}
+# char_list = []
+# num_list = []
+# new_num_list = []
+# new_char_list = []
+
+# for i in a:
+#       if i.isdigit():
+#             num_list.append(int(i))
+#       else:
+#             char_list.append(i)
+
+# for i in char_list:
+#       if i not in new_char_list:
+#             new_char_list.append(i)
+            
+# for i in range(0, len(new_char_list)):
+#       Sum = 0
+#       indexs = []       
+#       for u in range(0, len(char_list)):
+#             if new_char_list[i] == char_list[u]:
+#                   indexs.append(u)
+      
+#       for number in indexs:
+#             Sum += num_list[number]
+#       new_num_list.append(Sum)
+# for x, y in zip(new_char_list, new_num_list):
+#       mydict.setdefault(x,y)
+# print(mydict)
+
+
+    
+
+    
+    
